dd4e7f59eff777b87050e100fe82a11627eaa2bc
Simone Gentili <sensorario@gmail.com>
2023-05-14 22:55:04 +0200

    resolve #6: add code sniffer

--- src/functions.php
+++ src/functions.php
@@ -1,11 +1,36 @@
 <?php
+/**
+ * This is an automatic dependency injection function
+ * php version 8.2
+ * 
+ * @category Project
+ * @package  Sensorario\DependencyInjection
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  https://github.com/sensorario/dependency-injection/blob/main/LICENCE MIT
+ * @version  GIT: <v1.0.0>
+ * @link     https://github.com/sensorario/dependency-injection
+ */
 
 namespace Sensorario\DependencyInjection;
 
-function buildDeps($subject, $method, $deps = []) {
-    if (!method_exists($subject, '__construct')) return null;
+/**
+ * Build all the dependencies of a given subject. Subject
+ * could be a class or an object.
+ * 
+ * @param mixed  $subject the given class or instance
+ * @param string $method  the given method
+ * @param array  $deps    the list of dependencies
+ * 
+ * @return array
+ */
+function buildDeps($subject, $method, $deps = [])
+{
+    if (!method_exists($subject, '__construct')) {
+        return null;
+    }
+
     $params = (new \ReflectionMethod($subject, $method))
-        ->getParameters();
+    ->getParameters();
     foreach ($params as $dep) {
         $className = (string) $dep->getType();
         $deps[] = new $className;
@@ -13,7 +38,18 @@ function buildDeps($subject, $method, $deps = []) {
     return $deps;
 }
 
-function injector($instance, $method = '__construct') {
+/**
+ * Injection receive a class or an object and the relative method.
+ * Then returns the instance of the given class, and the value
+ * returned from the given methods.
+ * 
+ * @param mixed  $instance a class or object
+ * @param string $method   the given method
+ * 
+ * @return array
+ */
+function injector($instance, $method = '__construct')
+{
     $deps = buildDeps($instance, $method);
     return $method == '__construct'
         ? new $instance(...$deps)
